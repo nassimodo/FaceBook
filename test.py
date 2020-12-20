a96e10b9114335c5ceb42eb8dc9f8469064068bc
Nassim Derras <derrasnassim@gmail.com>
2020-12-20 17:32:58 +0100

    worked on the swipe calculation

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print('Hello World aka Nassim!)
+print('Hello World aka Nassim!')
+
+a = 2
+b = 6
+print('initial values')
+print('a = ', a, ' and b = ', b)
+
+c = b
+b = a
+a = c
+print('Swiped values')
+print('a = ', a, ' and b = ', b)
