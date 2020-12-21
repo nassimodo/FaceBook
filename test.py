8d519452eb7f2a1e80a560c9cce4490946e3d9a4
Nassim Derras <derrasnassim@gmail.com>
2020-12-21 10:47:49 +0100

    modification 2

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
-print('Hello World aka Nassim!')
+print('Hello World aka Nassim!)
 
 a = 2
 b = 6
+
 print('initial values')
-print('a = ', a, ' and b = ', b)
+print('a = ',a, ' and b = ',b)
 
 c = b
 b = a
-a = c
-print('Swiped values')
-print('a = ', a, ' and b = ', b)
+a = c 
+
+print('swiped values')
+print('a = ',a, ' and b = ',b)
